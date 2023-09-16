cd2c5ec32f9f669507e950ce60103e575b84cd34
Dhaine-Leader <dhaine_br@protonmail.com>
2023-09-16 15:56:34 +0800

    app routing fixed, generated all components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './MainNavigation/home/home.component';
+import { StoresComponent } from './MainNavigation/stores/stores.component';
+import { UsersComponent } from './MainNavigation/users/users.component';
+import { ArchiveComponent } from './MainNavigation/archive/archive.component';
+import { ReceptionComponent } from './MainNavigation/reception/reception.component';
+import { SettingsComponent } from './MainNavigation/settings/settings.component';
+
+
+const routes: Routes = [
+  { path: '', redirectTo:'login', pathMatch:'full'},
+  { path: 'login', component: LoginComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'archive', component: ArchiveComponent},
+  { path: 'stores', component: StoresComponent},
+  { path: 'reception', component: ReceptionComponent},
+  { path: 'users', component: UsersComponent},
+  { path: 'settings', component: SettingsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './MainNavigation/home/home.component';
+import { StoresComponent } from './MainNavigation/stores/stores.component';
+import { UsersComponent } from './MainNavigation/users/users.component';
+import { ArchiveComponent } from './MainNavigation/archive/archive.component';
+import { ReceptionComponent } from './MainNavigation/reception/reception.component';
+import { SettingsComponent } from './MainNavigation/settings/settings.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    StoresComponent,
+    UsersComponent,
+    ArchiveComponent,
+    ReceptionComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
